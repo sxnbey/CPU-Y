2827b06b28b756e5a1682130fd4e9473c1dfa0e2
senbey <nicci250404@gmail.com>
2024-08-20 13:14:49 +0200

    bob

--- modules/functions.js
+++ modules/functions.js
@@ -2,42 +2,30 @@
 *                                     DECLARATION & IMPORTS                                      *
 \************************************************************************************************/
 
-// Imports all needed modules to run the program.
+// Creates the functions object which is to be exported later.
 
-const createData = require("./modules/data.js");
-const createMenu = require("./modules/menu.js");
-const functions = require("./modules/functions.js");
-const errors = require("./data/errors.json");
-const system = { functions: { functions }, errors: errors };
-
-/************************************************************************************************\
-*                                              MAIN                                              *
-\************************************************************************************************/
-
-// Clears the console.
-
-console.clear();
-
-// Runs the program.
-
-run();
+const functions = {};
 
 /************************************************************************************************\
 *                                            EXPORTS                                             *
 \************************************************************************************************/
 
-// Exports the system object so I can use it in other files.
+// Exports the functions object.
 
-module.exports = system;
+module.exports = functions;
 
 /************************************************************************************************\
 *                                           FUNCTIONS                                            *
 \************************************************************************************************/
 
-// Creates the overlay with the imported data.
+functions.handleError = function (layer, error) {
+  const errors = require("../data/errors.json");
 
-async function run() {
-  const data = await createData();
+  error = errors[layer].find((i) => i.code == error);
 
-  createMenu(data);
-}
+  console.log("\n");
+  console.log("Beim Bearbeiten deiner Anfrage ist ein Fehler aufgetreten.");
+  console.log("\n");
+  console.log(layer + "x" + error.code);
+  console.log(error.name);
+};
