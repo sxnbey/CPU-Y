dadb76f450fae035699a62ff4f36be93fb49c630
senbeyxd <nicci250404@gmail.com>
2024-08-29 19:47:15 +0200

    noch mehr sauber machen und nicht weitermachen weil ich unfassbar faul bin es ist insane

--- index.js
+++ index.js
@@ -30,16 +30,18 @@ loader(system);
 console.log("\n");
 console.log("Please wait while CPU-Y is fetching your system information...");
 
-// Fetches the system information in an async function because I don't like then(). You'll never catch me using then(), that's a promise AHAHHAHHAHahha
+// Fetches the system information in an async function because I don't like then(). You'll never catch me using then(), that's a PROMISE!!!! AHAHHAHHAHahha
 
 (async () => {
   await system.modules.createData(system);
 
   if (process.stdout.columns < 47) return system.functions.winTooSmall();
 
-  // Runs the program.
+  // Runs the CPU-Y.
   system.functions.run();
 })();
+
+// This event is triggered when the size of the console window is changed.
 
 process.stdout.addListener("resize", () => {
   // Just so nothing gets messed up while CPU-Y is starting up.
